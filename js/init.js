52d438d1b7080731f245a26ffa15c1a4836e7605
yowjerry <ingjerrycenatus@gmail.com>
2022-06-28 21:24:15 -0400

    Tiz project

---
+++ js/init.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() { 
+    M.Sidenav.init(document.querySelectorAll('.sidenav'), null);
+    M.Carousel.init(document.querySelectorAll('.carousel'),null);
+  });
+  function download(){
+    alert("download started");
+  }
+  function sendMessage(name,message){
+    
+  }
