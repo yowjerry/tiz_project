42198140cee73df52206d53d0f2b61d2acfe47e1
yowjerry <ingjerrycenatus@gmail.com>
2022-07-30 14:04:14 -0400

    integration methode de paiement

--- js/init.js
+++ js/init.js
@@ -1,10 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() { 
-    M.Sidenav.init(document.querySelectorAll('.sidenav'), null);
-    M.Carousel.init(document.querySelectorAll('.carousel'),null);
-  });
-  function download(){
-    alert("download started");
-  }
-  function sendMessage(name,message){
-    
-  }
+document.addEventListener('DOMContentLoaded', function () {
+  M.Sidenav.init(document.querySelectorAll('.sidenav'), null);
+  M.Carousel.init(document.querySelectorAll('.carousel'), null);
+});
+function download() {
+  window.open("https://play.google.com/store/apps/details?id=com.tiz.yowjerryapps", '_blank').focus();
+}
+function sendMessage( message) {
+  message_=encodeURI(message);
+  url='https://wa.me/+50937771713?text='+message_;
+  window.open(url,'_blank');
+}
